5685bf2bd4a36b1a78be8e7a08177b1cc1d1a841
Adrian Gallardo <adrian.gallardo@wizeline.com>
2019-10-10 12:50:36 -0500

    fix: linter errors

--- src/getCookieCutterSource.js
+++ src/getCookieCutterSource.js
@@ -29,6 +29,14 @@ const extractSource = ({ source, destination }) =>
     });
   });
 
+function moveDirFilesSync(sourceDir, destination) {
+  const files = fs.readdirSync(sourceDir);
+
+  files.forEach((file) => {
+    fse.moveSync(`${sourceDir}/${file}`, `${destination}/${file}`);
+  });
+}
+
 const getCookieCutterSource = async ({ branchName, destination }) => {
   const source = getSource(branchName);
 
@@ -50,7 +58,7 @@ const getCookieCutterSource = async ({ branchName, destination }) => {
     source: destinationSourceFilename,
     destination,
   });
-  
+
   /*
     Code is cloned inside "frontend-cookiecutter-application-master".
     This extracts the content files to an upper level so the code
@@ -62,14 +70,6 @@ const getCookieCutterSource = async ({ branchName, destination }) => {
   fse.removeSync(`${destination}/frontend-cookiecutter-application-master`);
   fse.removeSync(`${destination}/master.zip`);
 };
-
-function moveDirFilesSync(sourceDir, destination) {
-  const files = fs.readdirSync(sourceDir);
-
-  files.forEach(function (file) {
-    fse.moveSync(`${sourceDir}/${file}`, `${destination}/${file}`);
-  });
-}
 
 export {
   getCookieCutterSource,
